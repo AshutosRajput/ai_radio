7121b3ff30eaceac98b3f7ca17615dba257ba135
AshutosRajput <rajashutosh491@gmail.com>
2023-07-21 22:54:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ai_radio/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+      debugShowCheckedModeBanner: false,
+      home:  HomePage(),
+    );
+  }
+}
